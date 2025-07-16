1df8c5e3b63bd1b209097f6129a25887120f2e7d
Shreyash Sahare <shsa7246@colorado.edu>
2025-07-16 00:19:40 -0600

    Modified syntheticdatageneration.py

--- syntheticdatageneration.py
+++ syntheticdatageneration.py
@@ -2,3 +2,15 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from colorama import Fore
 
+if __name__ == "__main__":
+    loader = PyPDFLoader("palantir_foundry_tech_doc.pdf")
+    docs = loader.load()
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=512,
+        chunk_overlap=64,
+        separators=["\n\n", "\n", " ", ""]
+    )
+    chunks = splitter.split_documents(docs)
+
+    
